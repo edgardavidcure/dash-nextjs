adbbc18b2b25c468367eb6304ef29a3cc5fb5b74
edgar <edgardavid1503@gmail.com>
2024-01-25 09:57:51 -0800

    corrected indentation of error handling elements

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -14,7 +14,6 @@ import { useFormState } from 'react-dom';
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState = { message: null, errors: {} };
   const [state, dispatch] = useFormState(createInvoice, initialState);
- 
   return (
     <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
@@ -66,14 +65,13 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              aria-describedby="customer-error"
+              aria-describedby="amount-error"
 
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
-        </div>
-        <div id="customer-error" aria-live="polite" aria-atomic="true">
+          <div id="amount-error" aria-live="polite" aria-atomic="true">
         {state.errors?.amount &&
           state.errors.amount.map((error: string) => (
             <p className="mt-2 text-sm text-red-500" key={error}>
@@ -81,6 +79,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             </p>
           ))}
       </div>
+        </div>
+        
 
         {/* Invoice Status */}
         <fieldset>
@@ -114,7 +114,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="paid"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
-              aria-describedby="customer-error"
+                  aria-describedby="status-error"
 
                 />
                 <label
@@ -124,7 +124,11 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   Paid <CheckIcon className="h-4 w-4" />
                 </label>
               </div>
-              <div id="customer-error" aria-live="polite" aria-atomic="true">
+              
+            </div>
+            
+          </div>
+          <div id="status-error" aria-live="polite" aria-atomic="true">
         {state.errors?.status &&
           state.errors.status.map((error: string) => (
             <p className="mt-2 text-sm text-red-500" key={error}>
@@ -132,9 +136,11 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             </p>
           ))}
       </div>
-            </div>
-            
-          </div>
+      <div aria-live="polite" aria-atomic="true">
+          {state.message ? (
+            <p className="mt-2 text-sm text-red-500">{state.message}</p>
+          ) : null}
+        </div>
         </fieldset>
       </div>
       <div className="mt-6 flex justify-end gap-4">
